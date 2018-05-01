04053bddfb2dd79897584d7b1866a678f5c10c8a
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    TP1803: overlay: Enable option for full screen aspect ratio
    Change-Id: I2a1b2acc5ed97ccb6795dc371c2b3ac9cb4f9d03

--- device.mk
+++ device.mk
@@ -12,7 +12,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Recovery
 PRODUCT_PACKAGES += \

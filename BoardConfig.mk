434faee0679a1f8f8e8103704e03690ceb1c399d
Arian <arian.kulmer@web.de>
2020-03-23 21:42:04 +0100

    TP1803: Configure architecture and platform
    Change-Id: I44382e290d6997fcc97f2b879c2b1c299703a572

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,29 @@
 
 DEVICE_PATH := device/nubia/TP1803
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msmnile
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msmnile
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+
 # Screen density
 TARGET_SCREEN_DENSITY := 480
 

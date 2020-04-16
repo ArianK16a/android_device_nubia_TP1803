baf3b4a5067780b6b9d39389d374539a3d8381b1
Arian <arian.kulmer@web.de>
2020-04-16 16:35:33 +0200

    TP1803: Create EGL symlinks
    Change-Id: I1a6d2460ec514e26ca7b3652133aa86032b5ac83

--- Android.mk
+++ Android.mk
@@ -31,6 +31,20 @@ $(DSP_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(BT_FIRMWARE_MOUNT_POINT) $(DSP_MOUNT_POINT)
 
+EGL_SYMLINKS := $(TARGET_OUT_VENDOR)/lib/
+$(EGL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	mkdir -p $@
+	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
+	$(hide) ln -sf egl/libGLESv2_adreno $@/libGLESv2_adreno
+
+EGL64_SYMLINKS := $(TARGET_OUT_VENDOR)/lib64/
+$(EGL64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	mkdir -p $@
+	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
+	$(hide) ln -sf egl/libGLESv2_adreno $@/libGLESv2_adreno
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINKS) $(EGL64_SYMLINKS)
+
 RFS_MDM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/mdm/adsp/
 $(RFS_MDM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MDM ADSP folder structure: $@"

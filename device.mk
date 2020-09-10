1d20622e1e60efc41fac128285df6e606518d6ac
Anay Wadhera <awadhera@berkeley.edu>
2020-09-10 00:59:31 -0700

    TP1803: Add custom lights HAL
    Change-Id: Ica2bedb627782d99d3893ab95b1321735d16fa9b

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.TP1803
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm

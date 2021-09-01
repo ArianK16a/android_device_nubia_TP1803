fe8fc42fcca3bf002fded876fc488933c5faccac
Arian <arian.kulmer@web.de>
2021-09-01 18:23:49 +0200

    TP1803: Add camera hardware permissions
    Change-Id: If6031ba144253cbba44bd99b38d9d970fcd58532

--- device.mk
+++ device.mk
@@ -81,6 +81,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libprotobuf-cpp-full-vendorcompat
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore

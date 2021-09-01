0cf535afec0db74ea6f9f77686ebd78e9d9a88a5
Arian <arian.kulmer@web.de>
2021-09-01 22:47:36 +0200

    TP1803: Import qti privapp permissions
    Change-Id: I0d0268e8eb70b496856f85d7d67f269e6286dd31

--- device.mk
+++ device.mk
@@ -148,7 +148,8 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 # Recovery
 PRODUCT_PACKAGES += \

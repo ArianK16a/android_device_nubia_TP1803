e8fca4c503f9ce4f3f1e6148fbba797211f59427
Arian <arian.kulmer@web.de>
2021-09-02 18:42:59 +0200

    TP1803: Build fingerprint service and import sunwave HAL
    Change-Id: I9bcf100be45fc92ac75fe598c0278d1ad805761a

--- device.mk
+++ device.mk
@@ -125,6 +125,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Handheld hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml

5544016ef2a1c1e3cbcbb66cd672ecf5870ba5db
Arian <arian.kulmer@web.de>
2021-09-03 10:17:38 +0200

    Revert "TP1803: Blob updates"
    This reverts commit e2d4d1157214c26b2b0a9003da4bdf071f64c108.

--- extract-files.sh
+++ extract-files.sh
@@ -55,9 +55,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib/libOmxVpp.so | vendor/lib/libvppclient.so)
-            "${PATCHELF}" --remove-needed "libhwbinder.so" "${2}"
-            ;;
     esac
 }
 

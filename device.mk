f646b2cc22a13d12629b7da582b2bc9c756de171
Arian <arian.kulmer@web.de>
2021-09-30 17:49:56 +0200

    Update blobs
    Change-Id: I8c33f9a70a52a1dba2830f1b726531093bed89d4

--- device.mk
+++ device.mk
@@ -186,6 +186,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm

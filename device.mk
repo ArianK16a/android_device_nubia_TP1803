057fccb8f0c9a5d3a67bda50401e00cb20e5ef8f
Arian <arian.kulmer@web.de>
2021-11-18 18:48:45 +0100

    TP1803: rootdir: Import usb scripts from LA.UM.9.1.r1-11100-SMxxx0.0
    Change-Id: Ib193a604c5ad525ab32800e697b75a03a50c5c76

--- device.mk
+++ device.mk
@@ -250,6 +250,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sdio.sh \
     init.qcom.sh \
     init.qcom.ssr.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
     init.qti.dcvs.sh \
     init.recovery.qcom.rc \
     init.target.rc \
